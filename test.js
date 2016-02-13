4df2fdd6b80fac6754a7d4f070bdacd138b6304e
Timofey Biryukov <timofeybiryukov@gmail.com>
2016-02-14 02:13:58 +0600

    testing types

--- test.js
+++ test.js
@@ -25,6 +25,19 @@ app.post('/testInjection', Injector.IC(function(foo, bar) {
 }));
 
 
+app.post('/testTypesInjection', Injector.IC(
+    function(_sting, _number, _boolean, _object) {
+  console.assert(typeof _sting === 'string');
+  console.assert(typeof _number === 'number');
+  console.assert(typeof _boolean === 'boolean');
+  console.assert(typeof _object === 'object');
+  this.res.end(_sting.toString() + ', ' +
+      _number.toString() + ', ' +
+      _boolean.toString() + ', ' +
+      _object.toString());
+}));
+
+
 app.listen(PORT);
 console.log('<--- Server lifted --->');
 console.log('http://127.0.0.1:' + PORT);
@@ -43,8 +56,8 @@ function test(res) {
     console.log(body);
     console.assert(body === 'bar, foo');
     console.log('</--- test ' + res.req.method +
-      ' ' + res.req.path +
-      ' --->');
+        ' ' + res.req.path +
+        ' --->');
   });
 }
 
@@ -55,6 +68,9 @@ function test(res) {
 req = http.get('http://127.0.0.1:' + PORT + '/testInjection?foo=bar&bar=foo', test);
 
 
+/**
+ * testing post body json injection
+ */
 req = http.request({
   hostname: '127.0.0.1',
   port: PORT,
@@ -67,5 +83,39 @@ req = http.request({
 req.write(JSON.stringify({
   'foo': 'bar',
   'bar': 'foo'
+}));
+req.end();
+
+
+req = http.request({
+  hostname: '127.0.0.1',
+  port: PORT,
+  path: '/testTypesInjection',
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json'
+  }
+}, function(res) {
+  console.log('<--- test ' + res.req.method +
+    ' ' + res.req.path +
+    ' --->');
+  console.log(res.statusCode);
+  res.setEncoding('utf8');
+  res.on('data', function(buffer) {
+    var body = buffer.toString();
+    console.log(body);
+    console.assert(body === '_stinrg, 1, true, [object Object]');
+    console.log('</--- test ' + res.req.method +
+        ' ' + res.req.path +
+        ' --->');
+  });
+});
+req.write(JSON.stringify({
+  _sting: '_stinrg',
+  _number: 1,
+  _boolean: true,
+  _object: {
+    'foo': 'bar'
+  }
 }));
 req.end();
